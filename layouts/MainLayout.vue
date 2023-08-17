62a8cb93cd5278cf96c97ca67be77984160e577d
Marat <evolution99@rambler.ru>
2023-08-18 00:27:02 +0300

    Edited Mainlayout.vue in layouts folder

--- layouts/MainLayout.vue
+++ layouts/MainLayout.vue
@@ -12,8 +12,10 @@
 
         <div id="BottomNav" class="fixed z-50 bottom-0 flex w-full h-[70px] border-t border-t-gray-700 bg-black">
 
-            <div >
-                
+            <div class="flex w-full max-w-[500px] mx-auto flex items-center justify-around">
+                <button @click="navigateTo('/')" class="w-full h-full">
+                    <Icon name="material-symbols:home-outline" size="35" color="#ffffff" />
+                </button>
             </div>
         </div>
     </div>
