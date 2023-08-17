b6d3f678256162e8f885948394f5d84d24a61010
Marat <evolution99@rambler.ru>
2023-08-17 23:43:05 +0300

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <MainLayout>
+            INDEX
+        </MainLayout>
+    </div>
+</template>
+
+<script setup>
+import MainLayout from '~/layouts/MainLayout.vue';
+</script>
