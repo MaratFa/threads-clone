876827c3fae0795ed6c8c34f59175a4b3810a9f2
Marat <evolution99@rambler.ru>
2023-08-18 23:23:34 +0300

    Edited index.vue in pages folder

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,13 @@
 <template>
     <div>
         <MainLayout>
-            INDEX
+            <div id="IndexPage" class="w-full overflow-auto">
+                <div class="mx-auto max-w-[500px] overflow-hidden">
+                    <div id="Posts" class="px-4 max-w-[600px] mx-auto">
+
+                    </div>
+                </div>
+            </div>
         </MainLayout>
     </div>
 </template>
