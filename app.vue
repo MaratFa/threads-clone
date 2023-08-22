244c8cad2bd8679fd75c174deef0b4a77e08bcd0
Marat <evolution99@rambler.ru>
2023-08-22 07:41:16 +0300

    Edited app.vue in root folder

--- app.vue
+++ app.vue
@@ -2,10 +2,19 @@
   <div class="bg-black fixed w-[calc(100%+1px)] h-full">
     <NuxtPage />
 
-    <CreatePost :class="[
-      { 'max-h-[100vh] transition-all duration-200 ease-in visible': userStore.isMenuOverlay },
-      { 'max-h-0 transition-all duration-200 ease-out invisible': !userStore.isMenuOverlay }
-    ]" />
+    <CreatePost 
+      :class="[
+        { 'max-h-[100vh] transition-all duration-200 ease-in visible': userStore.isMenuOverlay },
+        { 'max-h-0 transition-all duration-200 ease-out invisible': !userStore.isMenuOverlay }
+      ]"
+    />
+
+    <Modal 
+      :class="[
+        { 'max-h-[100vh] transition-all duration-200 ease-in visible': userStore.isLogoutOverlay },
+        { 'max-h-0 transition-all duration-200 ease-out invisible': !userStore.isLogoutOverlay }
+      ]"
+    />
   </div>
 </template>
 
