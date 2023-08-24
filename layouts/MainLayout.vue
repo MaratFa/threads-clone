0d91e7c033fbb1cf3138de268f1254e23694eb91
Marat <evolution99@rambler.ru>
2023-08-24 05:29:59 +0300

    Edited

--- layouts/MainLayout.vue
+++ layouts/MainLayout.vue
@@ -2,7 +2,7 @@
     <div id="MainLayout" class="h-screen">
         <div class="w-full max-w-[500px] mx-auto">
             <div id="TopMenu" class="w-full flex items-center justify-center px-2 h-10 my-2">
-                <img class="w-[35px]" src="../public/threads-logo.png" />
+                <img class="w-[35px]" src="/threads-logo.png">
             </div>
         </div>
 
@@ -10,28 +10,28 @@
             <slot />
         </div>
 
-        <div 
-            id="BottomNav"
+        <div
+            id="BottomNav" 
             class="fixed z-50 bottom-0 flex w-full h-[70px] border-t border-t-gray-700 bg-black"
         >
             <div class="flex w-full max-w-[500px] mx-auto flex items-center justify-around">
-                <button
+                <button 
                     @click="navigateTo('/')"
                     class="w-full h-full"
                 >
-                    <Icon name="material-symbols:home-outline" size="35" color="#ffffff" />
+                    <Icon name="material-symbols:home-outline" size="35" color="#ffffff"/>
                 </button>
                 <button 
                     @click="userStore.isMenuOverlay = true"
                     class="w-full h-full"
                 >
-                    <Icon class="mb-1" name="material-symbols:edit-square-outline-rounded" size="32" color="ffffff" />
+                    <Icon class="mb-1" name="material-symbols:edit-square-outline-rounded" size="32" color="#ffffff"/>
                 </button>
                 <button 
                     @click="userStore.isLogoutOverlay = true"
                     class="w-full h-full"
                 >
-                    <Icon class="mb-1" name="ph:sign-out" size="32" color="#ffffff" />
+                    <Icon class="mb-1" name="ph:sign-out" size="32" color="#ffffff"/>
                 </button>
             </div>
         </div>
@@ -40,5 +40,8 @@
 
 <script setup>
 import { useUserStore } from '~/stores/user';
-const userStore = useUserStore();
+const userStore = useUserStore()
+
+const client = useSupabaseClient()
+const user = useSupabaseUser()
 </script>
