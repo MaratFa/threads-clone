70e70254f3b732caed854a81cddd42eebf1ea8ff
Marat <evolution99@rambler.ru>
2023-08-24 05:30:23 +0300

    Edited

--- server/api/like-post.js
+++ server/api/like-post.js
@@ -1,15 +1,15 @@
-import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
 
-export default defineEventHandker(async (event) => {
-  const body = await readBody(event);
+export default defineEventHandler(async (event) => {
+    const body = await readBody(event)
+        
+    const res = await prisma.likes.create({
+        data: {
+            userId: body.userId,
+            postId: body.postId,
+        }
+    })
 
-  const res = await prisma.likes.create({
-    data: {
-      userId: body.userId,
-      postId: body.postId,
-    },
-  });
-
-  return res;
-});
+    return res
+})
