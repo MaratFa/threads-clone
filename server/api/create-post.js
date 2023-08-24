fac3c3b284ddd5742e514c34b73f3d3943dcb5c9
Marat <evolution99@rambler.ru>
2023-08-24 05:30:17 +0300

    Edited

--- server/api/create-post.js
+++ server/api/create-post.js
@@ -1,18 +1,20 @@
-import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
 
-export default defineEventHandker(async (event) => {
-  const body = await readBody(event);
+export default defineEventHandler(async (event) => {
+    const body = await readBody(event)
 
-  const res = await prisma.posts.create({
-    data: {
-      userId: body.userId,
-      name: body.name,
-      image: body.image,
-      text: body.text,
-      picture: body.picture,
-    },
-  });
+    console.log(body)
+    
+    const res = await prisma.posts.create({
+        data: {
+            userId: body.userId,
+            name: body.name,
+            image: body.image,
+            text: body.text,
+            picture: body.picture,
+        }
+    })
 
-  return res;
-});
+    return res
+})
